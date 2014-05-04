cfdbe6f2a5cc91e7b7b1dbfbf3a77f091097d655
mikan-megane <kanata702@gmail.com>
2014-05-04 11:49:07 +0900

    nicoge-mocha

--- config.rb
+++ config.rb
@@ -6,14 +6,13 @@ css_dir = "stylesheets"
 sass_dir = "scss"
 images_dir = "images"
 javascripts_dir = "javascripts"
-http_images_dir = "images/"
 
 # You can select your preferred output style here (can be overridden via the command line):
-output_style = :expanded
-#  or :nested or :compact or :compressed
+output_style = :compressed
+# :expanded or :nested or :compact or :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-relative_assets = false
+# relative_assets = false
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 line_comments = false
