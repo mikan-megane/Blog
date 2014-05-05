ce310f24b4081563962d9307db356477cee2b499
mikan-megane <kanata702@gmail.com>
2014-05-05 14:57:32 +0900

    javascript

---
+++ javascripts/user.js
@@ -0,0 +1,18 @@
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js"></script>
+<div style="text-align:left;" class="nicotan"><span style="color:#333333;">誕生日</span><br /><script type="text/javascript" src="http://coels.net/kt/script_g/ikutu-g21.js?byear=2010&bmonth=6&bdate=1&acolor=#333333&mark=1" charset="shift-jis"></script></div>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  ga('create', 'UA-30892461-1', 'ameblo.jp');
+  ga('send', 'pageview');
+</script>
+<link rel="shortcut icon" href="http://stat001.ameba.jp/user_images/20120318/12/nicoge-mocha/3f/11/g/o0016001611858633900.gif" type="image/x-icon" />
+<link rel="icon" href="http://stat001.ameba.jp/user_images/20120318/12/nicoge-mocha/3f/11/g/o0016001611858633900.gif" type="image/gif" />
+<script type="text/javascript">$(function(){$("#ArticleFooter").appendTo(".articleText")});
+</script>
+<!--[if lt IE 9]>
+<script src="http://ie7-js.googlecode.com/svn/version/2.1(beta4)/IE9.js"></script>
+<![endif]-->
