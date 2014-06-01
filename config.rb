e764884da3535988ebe3f288d603c402d3a8c00c
mikan-megane <kanata702@gmail.com>
2014-06-01 22:46:05 +0900

    ver 1.0.1

--- config.rb
+++ config.rb
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = "https://rawgit.com/mikan-megane/Blog-test/master/"
+http_path = "https://rawgit.com/mikan-megane/Blog/master/"
 css_dir = "stylesheets"
 sass_dir = "scss"
 images_dir = "images"
@@ -24,31 +23,3 @@ line_comments = false
 # preferred_syntax = :sass
 # and then run:
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
-=======
-# Require any additional compass plugins here.
-
-# Set this to the root of your project when deployed:
-http_path = "https://rawgit.com/mikan-megane/Blog/master/"
-css_dir = "stylesheets"
-sass_dir = "scss"
-images_dir = "images"
-javascripts_dir = "javascripts"
-
-# You can select your preferred output style here (can be overridden via the command line):
-output_style = :compressed
-# :expanded or :nested or :compact or :compressed
-
-# To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = false
-
-# To disable debugging comments that display the original location of your selectors. Uncomment:
-line_comments = false
-
-
-# If you prefer the indented syntax, you might want to regenerate this
-# project again passing --syntax sass, or you can uncomment this:
-# preferred_syntax = :sass
-# and then run:
-# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
-
->>>>>>> 633b3dd5325bf5aadee46a4a4efe35fd498022ad
