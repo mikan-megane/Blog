85fb12e29ac8673d0ef97d4390170fc00bd0f0df
mikan-megane <kanata702@gmail.com>
2014-06-02 20:50:08 +0900

    ver 1.0.3
    記事下のリンク変更
    怒涛の修正も落ち着くはず

--- javascripts/scroll.js
+++ javascripts/scroll.js
@@ -2,9 +2,9 @@
 (function($){
 $(window).scroll(function () { if ($(this).scrollTop() > 100) {
 $(".skinHeaderArea").stop().animate({"top": "-110px","left": "-20px","width": "180px"}, 300,'swing');
-$(".skinHeaderArea2").stop().animate({"top": "115px"}, 300,'swing');
+$(".skinHeaderArea2").stop().animate({"top": "120px","backgroundSize":"80%","left":"17px"}, 300,'swing');
 $(".skinBlogHeadingGroupArea").stop().animate({"top": "-165px","opacity": "0"}, 300,'swing');} else {
 $(".skinHeaderArea").stop().animate({"top": "30px","left": "0px","width": "200px"}, 300,'swing');
-$(".skinHeaderArea2").stop().animate({"top": "0px"}, 300,'swing');
+$(".skinHeaderArea2").stop().animate({"top": "0px","backgroundSize":"88%","left":"0px"}, 300,'swing');
 $(".skinBlogHeadingGroupArea").stop().animate({"top": "0px","opacity": "1"}, 300,'swing');}});}(jQuery));
 </script>
