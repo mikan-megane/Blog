5196d309f1fbc94e07541344e782fd188ef807b4
mikan-megane <kanata702@gmail.com>
2014-06-02 19:40:49 +0900

    ver 1.0.2
    サイドバーのtext-indent修正

--- javascripts/scroll.js
+++ javascripts/scroll.js
@@ -1,11 +1,10 @@
 <script type="text/javascript">
 (function($){
-	$(window).scroll(function () {
-		if ($(this).scrollTop() > 100) {
-			$("#ambHeader").stop().animate({"top": "-30px"}, 100);
-		} else {
-			$("#ambHeader").stop().animate({"top": "0px"}, 100);
-		}
-	});
-}(jQuery));
+$(window).scroll(function () { if ($(this).scrollTop() > 100) {
+$(".skinHeaderArea").stop().animate({"top": "-110px","left": "-20px","width": "180px"}, 300,'swing');
+$(".skinHeaderArea2").stop().animate({"top": "115px"}, 300,'swing');
+$(".skinBlogHeadingGroupArea").stop().animate({"top": "-165px","opacity": "0"}, 300,'swing');} else {
+$(".skinHeaderArea").stop().animate({"top": "30px","left": "0px","width": "200px"}, 300,'swing');
+$(".skinHeaderArea2").stop().animate({"top": "0px"}, 300,'swing');
+$(".skinBlogHeadingGroupArea").stop().animate({"top": "0px","opacity": "1"}, 300,'swing');}});}(jQuery));
 </script>
